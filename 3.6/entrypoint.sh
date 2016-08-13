f3eb1a2d952d57bcdd62de3759ed4335b91694ce
Roman Mohr <rmohr@redhat.com>
2016-08-13 22:09:16 +0200

    Preserve PKI files when deploying on Kubernetes

--- 3.6/entrypoint.sh
+++ 3.6/entrypoint.sh
@@ -12,5 +12,8 @@ echo OVESETUP_CONFIG/fqdn=str:$OVIRT_FQDN >> answers.conf
 echo OVESETUP_CONFIG/adminPassword=str:$OVIRT_PASSWORD >> answers.conf
 echo OVESETUP_PKI/organization=str:$OVIRT_PKI_ORGANIZATION >> answers.conf
 
+# Copy pki template files into original template location.
+# Mounts on kubernetes hide the original files in the image.
+cp -a /etc/pki/ovirt-engine.tmpl/* /etc/pki/ovirt-engine/
 
 engine-setup --config=answers.conf --offline && su -m -s /bin/python ovirt /usr/share/ovirt-engine/services/ovirt-engine/ovirt-engine.py start
