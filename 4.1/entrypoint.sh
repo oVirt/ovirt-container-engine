183f3d10a5413c412b96933ee48551e6dd98910f
root <root@dhcp-1-110.tlv.redhat.com>
2017-03-02 14:29:24 +0200

    change file names
    Change-Id: I152ef38a3fc2f8c9047275c59f2dfe0362b0a1af
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- 4.1/entrypoint.sh
+++ 4.1/entrypoint.sh
@@ -41,6 +41,7 @@ echo "ENGINE_SSO_SERVICE_URL=\"https://${OVIRT_FQDN}:${HTTPS_PORT}/ovirt-engine/
 echo "ENGINE_BASE_URL=\"https://${OVIRT_FQDN}:${HTTPS_PORT}/ovirt-engine/\"" >> /etc/ovirt-engine/engine.conf.d/999-ovirt-engine.conf
 echo "SSO_ENGINE_URL=\"https://${OVIRT_FQDN}:${HTTPS_PORT}/ovirt-engine/\"" >> /etc/ovirt-engine/engine.conf.d/999-ovirt-engine.conf
 
+echo "ENGINE_SSO_SERVICE_URL=\"https://${OVIRT_FQDN}:${HTTPS_PORT}/ovirt-engine/sso\"" >> /etc/ovirt-engine/engine.conf.d/11-setup-sso.conf
 export PGPASSWORD=$POSTGRES_PASSWORD
 
 psql $POSTGRES_DB -h $POSTGRES_HOST -p $POSTGRES_PORT  -U $POSTGRES_USER -c "UPDATE vdc_options set option_value = '$HOST_INSTALL' where option_name = 'InstallVds';"
