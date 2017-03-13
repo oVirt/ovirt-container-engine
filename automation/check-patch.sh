169cc48e8c9fe1a95fa03dda2956c056691c5301
Daniel Belenky <dbelenky@redhat.com>
2017-03-13 16:39:06 +0200

    Test docker build in stdci
    Change-Id: I94519d7c2b083970eb905434c4e86b48329c6f47
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,2 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 # TODO validate Dockerfile syntax
+# This code should be replaced with a real verification.
+# Currently, just checking docker build in STD-CI
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+CNAME="ovirt/engine-monolithic"
+OVERSION="4.0"
+SUFFIX="$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+TAGV=${OVERSION}-${SUFFIX}
+
+cd ${OVERSION}
+docker build --no-cache --pull -t ${CNAME}:${TAGV} -f Dockerfile .
+docker save ${CNAME}:${TAGV} | gzip > ../exported-artifacts/${CNAME/\//-}-${TAGV}.tar.gz
