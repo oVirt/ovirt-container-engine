aa4cdce0e84195c456d235356e4499c436566a47
Yaniv Bronhaim <ybronhei@redhat.com>
2017-03-15 14:58:38 +0200

    node image: Moving configure run to deploy script
    lvm configurator uses systemd, we can't run the configure logic as part
    of creating the container.
    
    Change-Id: If3b08945ba457e009651414255966fa3bf2d8c24
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- image-specifications/node/scripts/deploy.sh
+++ image-specifications/node/scripts/deploy.sh
@@ -3,11 +3,12 @@
 # Waiting until vdsmd is up.. better to check service status before running
 sleep 10
 
+vdsm-tool configure --force
+
 # setting link name - vdsm recognize veth_* prefix as veth type device
 ip link set eth0 down
 ip link set eth0 name veth_name0
 ip link set veth_name0 up
-# in our cluster 10.32.0.1 is the default gateway
 ip route add default via $CLUSTER_GATEWAY dev veth_name0
 #
 
